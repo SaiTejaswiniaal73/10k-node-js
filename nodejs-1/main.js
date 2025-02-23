be601c140b9e08c16bd2575e175f04a5bc3e9dc1
SaiTejaswiniaal73 <saitejaswini.aal@gmail.com>
2025-02-23 21:00:10 +0530

    yes

--- nodejs-1/main.js
+++ nodejs-1/main.js
@@ -1,68 +1,141 @@
-
 const http = require("http");
-const url = require("url"); // Required for query parsing
+const url = require("url");
+const fs = require("fs");
+// const { console } = require("inspector");
 
-const server = http.createServer((req, res) => {
-  const parsedURL = url.parse(req.url, true);
+// const server=http.createServer((req,res)=>{
+//   if(req.method=="POST"){
+//     req.on("data",(x)=>{
+//       console.log(x);
+//       res.end()
+//     })
+//   }
+// })
+// server.listen(3101, () => {
+//   console.log("Server running on port 3101");
+// });
+// // by using above data we will get juyst buffer data to get corcr data we need to write below
+// // so to cut binary pieces in small it is calles as chunk
 
-  console.log(parsedURL);
-  const tickets = parsedURL.query.tickets;
+// const http = require("http");
+// const url = require("url");
 
-//   console.log("write or slect the dish sweet or hot in website")
-  if (req.method === "GET") {
-    if (parsedURL.pathname === "/CHAVVA") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.write(
-        JSON.stringify({
-          MOVIE: "CHAVVA",
-          SCREEN: "ONE",
-          numberoftickets: tickets,
-        })
-      );
-      res.end();
-    } else if (parsedURL.pathname === "/THANDEL") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.write(
-        JSON.stringify({
-          MOVIE: "THANDEL",
-          SCREEN: "TWO",
-          numberoftickets: tickets,
-        })
-      );
-      res.end();
-    } else if (parsedURL.pathname === "/SKYFORCE") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.write(
-        JSON.stringify({
-          MOVIE: "SKYFORCE",
-          SCREEN: "THREE",
-          numberoftickets: tickets,
-        })
-      );
-      res.end();
-    } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.write(
-        JSON.stringify({ error: "Movie not found", numberoftickets: tickets })
-      );
-      res.end();
-    }
-  } else if (parsedURL.pathname === "POST") {
-    res.end();
-  } else if (req.method === "PUT") {
-    res.end();
-  } else if (req.method === "PATCH") {
-    res.end();
-  } else if (req.method === "DELETE") {
-    res.end();
-  } else {
-    res.end();
-  }
-});
-
+// const server = http.createServer((req, res) => {
+//   if (req.method == "POST") {
+//     let body = "";
+//     req.on("data", (chunk) => {
+//       body += chunk.toString();
+//       console.log(body);
+//       res.end();
+//     });
+//     req.on("error", (err) => {
+//       console.log(err);
+//       res.write("err");
+//     });
+//     req.on("end", () => {
+//       res.end("data receied");
+//     });
+//   }
+// });
 // server.listen(3101, () => {
 //   console.log("Server running on port 3101");
 // });
 
-function demo(a) {}
-demo(5);
+// // the above codes used to get data from binary to text
+
+// const server = http.createServer((req, res) => {
+//   if (req.method == "GET") {
+
+//     fs.readFile("./sample.txt","utf8",(err,data)=>{
+//       if(err){
+//         console.log(err)
+//         res.write(err)
+//         res.end();
+//       }
+//       else{
+//         console.log(data)
+//         res.write(data)
+//         res.end();
+
+//       }
+//     })
+//     fs.readFile("./info.txt","utf8",(err,data)=>{
+//       if(err){
+//         console.log(err)
+//         res.write(err)
+//         res.end()
+//       }
+//       else{
+//         console.log(data)
+//         res.write(data)
+//         res.end()
+
+//       }
+//     })
+
+//   }
+// });
+// server.listen(3105, () => {
+//   console.log("Server running on port 3101");
+// });
+// // the above code is related to asyncrhonus way
+
+// const server = http.createServer((req, res) => {
+//   if (req.method == "GET") {
+//     let sample_data=fs.readFileSync("./sample.txt","utf8")
+//      console.log(sample_data)
+
+//     let infro_data=fs.readFileSync("./info.txt","utf8")
+//     console.log(infro_data)
+//      res.end(sample_data + "\n" + infro_data);
+
+//   }
+// });
+// server.listen(3105, () => {
+//   console.log("Server running on port 3105");
+// });
+
+// // the above code is synchrons
+
+// // to add the dat to the file
+
+// const server = http.createServer((req, res) => {
+//   if (req.method == "POST") {
+//     let ipData = "10k coders";
+//     let sample_data = fs.writeFile("./sample.txt", ipData, (err) => {
+//       if (err) {
+//         res.end(err);
+//       } else {
+//         res.write("data inserted");
+//         res.end();
+//       }
+//     });
+//     console.log(sample_data);
+
+//     let infro_data = fs.readFileSync("./info.txt", "utf8");
+//     console.log(infro_data);
+//   }
+// });
+// server.listen(3105, () => {
+//   console.log("Server running on port 3105");
+// });
+// // the belwo code is for write in this also we have 2 fs.writeFile()  and fs,writeflrsync() it will remove the data whihc is there and repalce wirh aboev data
+
+const server = http.createServer((req, res) => {
+  if (req.method == "POST") {
+    let ipData = "10k coders";
+    fs.appendFile("./sample.txt", ipData, (err) => {
+      if (err) {
+        console.log(err)
+        res.end(err);
+      } else {
+        // res.write("data inserted");
+        res.end("data appended");
+      }
+    });
+   
+  }
+});
+server.listen(3105, () => {
+  console.log("Server running on port 3105");
+});
